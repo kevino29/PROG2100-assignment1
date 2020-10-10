96897c08f5acd365cb177b057840c81fbbbc8f0b
kevino29 <varax297@gmail.com>
2020-10-10 16:59:27 -0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string message = "Hello, World!";
+    cout << message << endl;
+    return 0;
+}
