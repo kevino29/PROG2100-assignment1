2ccc5389fa2be9f2be9781d5104155c963045105
kevino29 <varax297@gmail.com>
2020-10-12 15:26:51 -0300

    added original cpp file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,29 @@
 #include <string>
 #include <conio.h>
 #include <cctype>
+#include <fstream>
 
 using namespace std;
+
+// Prototypes
+void openCloseStreams();
+void openReadFile();
 
 int main() {
     string message = "Hello, World!";
     cout << message << endl;
+    cout << "Open File and Read it." << endl;
     return 0;
+}
+
+void openCloseStreams() {
+    // Declare stream objects
+    ifstream inStream;
+    ofstream outStream;
+    // Connect stream objects to file(s)
+
+}
+
+void openReadFile() {
+
 }
